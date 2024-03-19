54c9649d47b8ceb148fa60369fc2c6c51fb61f58
Alexey Yakovlev <mrlexus21@yandex.ru>
2024-03-19 23:08:56 +0300

    Create first service

---
+++ src/Controller/BookCategoryController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\BookCategoryService;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use OpenApi\Annotations as OA;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BookCategoryController extends AbstractController
+{
+    public function __construct(private BookCategoryService $bookCategoryService)
+    {
+    }
+
+    /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns book categories",
+     *     @Model(type=BookCategoryListResponse)
+     * )
+     */
+    #[Route(path: '/api/v1/book/categories', methods: ['GET'])]
+    public function categories(): Response
+    {
+        return $this->json($this->bookCategoryService->getCategories());
+    }
+}
